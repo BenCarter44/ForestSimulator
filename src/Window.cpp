5f37557b71acde9e1e3136ae06bc79492be61e48
Benjamin Carter <foothillben@gmail.com>
2023-10-13 16:34:32 -0700

    Wall with Shadow

--- src/Window.cpp
+++ src/Window.cpp
@@ -95,7 +95,7 @@ void Window::launch()
     glMatrixMode(GL_PROJECTION);
     DEBUG_WIN(gluOrtho2D(0,1000, 750, 0));
     gluOrtho2D(0,1000, 750, 0);
-    //gluOrtho2D(302,700, 416, 207);
+    //gluOrtho2D(45,955, 750, 408);
 
     DEBUG_WIN(glutDisplayFunc(Window::mainDisplay));
     glutDisplayFunc(Window::mainDisplay);
